5bc6705a5ad09be652e3b2421eea689db4eb9c2b
Bolotjanovna <toksonbaeva2104@gmail.com>
2023-10-12 01:09:22 +0500

    Test junit4 and testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,25 +1,24 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class CashbackHackServiceTest {
 
-    @Test
+    @org.testng.annotations.Test
     public void remain() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 1;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
-    @Test
+    @org.testng.annotations.Test
     public void boundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int actual = service.remain(amount);
         int expected = 100;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 }
